56e75772aaddefeab7db154e033faf864a616441
DOVENE AGOGUE <adovene@gmail.com>
2024-04-21 08:03:04 +0200

    final commit

--- src/main/java/com/dovene/tripbook/model/Trip.java
+++ src/main/java/com/dovene/tripbook/model/Trip.java
@@ -1,9 +1,6 @@
 package com.dovene.tripbook.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.*;
 
 @Entity
